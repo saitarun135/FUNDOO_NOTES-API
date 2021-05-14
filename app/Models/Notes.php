184b587ece4f1f64b49d0f1c6f7036c99c1755dd
saitarun135 <saitarun135@gm.com>
2021-05-14 16:04:34 +0530

    Notes model

---
+++ app/Models/Notes.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notes extends Model
+{
+    use HasFactory;
+    protected $fillable=['title','body'];
+    
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'updated_at'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+}
