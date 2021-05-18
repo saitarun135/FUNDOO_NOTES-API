9e0af46001a144e8fff3569a58fa8aab3cc8bffe
saitarun135 <saitarun135@gm.com>
2021-05-18 11:54:04 +0530

    added restrictions to the users & completed CRUD api

--- app/Http/Controllers/api/NotesController.php
+++ app/Http/Controllers/api/NotesController.php
@@ -1,9 +1,12 @@
 <?php
-
+//-->http error coded -->https://www.tutorialspoint.com/http/http_status_codes.htm
 namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Notes;
+use App\Models\User;
 use App\Http\Resources\Note as NoteResource;
 use Facade\FlareClient\Http\Response;
 use Illuminate\Auth\Events\Validated;
@@ -12,7 +15,67 @@ use App\Http\Requests;
 
 class NotesController extends Controller
 {
-  /* 
+    
+    public function display_All()
+    {
+        $notes=Notes::all();
+        return new NoteResource($notes);
+        //return Notes::find('id',5)->notes;
+    }
+
+    public function create_Note(Request $request)
+    {
+        $note=new Notes();
+        $note->title=$request->input('title');
+        $note->body=$request->input('body'); 
+        $note->user_id = auth()->id();          //userid added automatically from the usertable to NotesTable 
+        $note->save();
+        return new NoteResource($note);
+    }
+
+    public function display_Note($id)
+    {
+        $note=Notes::findOrFail($id);
+        if($note->user_id==auth()->id())
+            return new NoteResource($note);
+        else{
+            return response()->json([
+                'error' => 'UnAuthorized'], 401);
+                //echo "unauthorized";
+            }
+    }
+
+
+    public function update_Note(Request $request, $id)
+    {
+        $note=Notes::findOrFail($id);
+        if($note->user_id==auth()->id()){
+            $note->title=$request->input('title');
+            $note->body=$request->input('body');
+            $note->save();
+            return new NoteResource($note);
+        }
+        else
+        {
+            echo "Note is not available with that id";
+        }
+    }
+
+    public function delete_Note($id)
+    {
+        $note=Notes::findOrFail($id);
+        if($note->user_id==auth()->id()){
+            if($note->delete()){
+                return response()->json(['message'=>'Deleted'],201);
+                //return new NoteResource($note);
+            }
+        }
+        else{
+            return response()->json([
+                'error' => ' Method Not Allowed/invalid note id'], 405);
+        }
+    }
+ /* 
     protected $user;
     public function __construct()
     {
@@ -20,74 +83,14 @@ class NotesController extends Controller
         $this->user = auth()->user();   
     }
    */
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-    */
-
-    public function display_All()
-    {
-        $notes=Notes::all();
-        return new NoteResource($notes);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function create_Note(Request $request)
-    {
-        $note=new Notes();
-        $note->title=$request->input('title');
-        $note->body=$request->input('body');
-        $note->save();
-        return new NoteResource($note);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function display_Note($id)
-    {
-        $note=Notes::findOrFail($id);
-        return new NoteResource($note);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update_Note(Request $request, $id)
-    {
-        $note=Notes::findOrFail($id);
-        $note->title=$request->input('title');
-        $note->body=$request->input('body');
-        $note->save();
-        return new NoteResource($note);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function delete_Note($id)
-    {
-        $note=Notes::findOrFail($id);
-        if($note->delete()){
-            return new NoteResource($note);
-        }
-        
-    }
+//   public function list(){
+//            //echo "All notes";     
+//           //return Notes::all();
+//        //  return Notes::find(4)->user;
+//         // return User::all();
+//        // return User::find(30);
+//       // return Notes::all();
+//       // return Notes::find(32)->noteses;
+//       return User::find(32)->noteses;
+//   }
 }
