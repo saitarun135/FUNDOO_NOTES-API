c9e68bdc29360bc04da047d9f7a926d26e37702c
saitarun135 <saitarun135@gm.com>
2021-05-24 12:42:14 +0530

    [added]password reset link added

--- resources/views/Email/passwordReset.blade.php
+++ resources/views/Email/passwordReset.blade.php
@@ -1,14 +1,15 @@
 
 @component('mail::message')
-Hai ,
+Hello ,
 <br>
+{{-- This is your resetToken --}}
+{{-- {{$token}} --}}
+{{-- <br> --}}
+ click here to change your  <a href="http://localhost:3000/reset/{{$token}}">password</a> 
+
+Do not share it with any one..!
 {{$email}}
 <br>
-This is your resetToken
-{{$token}}
-<br>
-Do not share it with any one..!
-
 Thanks,<br>
 {{ config('app.name') }}
 @endcomponent
