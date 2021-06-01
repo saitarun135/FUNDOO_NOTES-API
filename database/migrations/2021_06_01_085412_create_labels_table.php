ece1451aeaf9e26b44de04f99daf7ff6fb0e0557
saitarun135 <saitarun135@gm.com>
2021-06-01 15:50:21 +0530

    [build]:migration table for labels model

---
+++ database/migrations/2021_06_01_085412_create_labels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLabelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('labels', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('label');
+            $table->unsignedBigInteger('user_id'); 
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
+
+            //making userid + label as a unique key combinely
+            $table->unique(['user_id','label']); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('labels');
+    }
+}
